fa076f447773204789689c63b812090a33ca47f6
newstmd <tangxin@qq.com>
2015-03-04 15:13:37 +0800

    20150304-1

--- lingxiu/ViewController.swift
+++ lingxiu/ViewController.swift
@@ -13,12 +13,42 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var (title,jieshuo) = readPlistMessage("0101")
+        print("here")
+        print("title:\(title)jieshuo:\(jieshuo)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //通过日期格式"0101"获得当天的内容
+    func readPlistMessage(dateString:String)->(String,String){
+        var urlPath = "http://219.153.125.103:12121/gethmgq.aspx?datekey=0101"
+        var url = NSURL(string: urlPath)
+        var data = NSData(contentsOfURL: url!, options: NSDataReadingOptions.DataReadingUncached, error: nil)
+        var json:AnyObject = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil)!
+        
+        var title = json.valueForKey("title") as String
+        var jieshuo = json.valueForKey("jieshuo") as String
+        //print("title:\(title)jieshuo:\(jieshuo)")
+        
+        
+//        var session = NSURLSession.sharedSession()
+//        var data:NSData?
+//        var task = session.dataTaskWithURL(url!, completionHandler: { (data, response , error) -> Void in
+//            
+//            if((error) != nil){
+//                print(error.localizedDescription)
+//            }
+//            
+//        })
+//        var err: NSError?
+//        var jsonResult = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
+//        
+//        println("my Data is \(jsonResult)")
+        return (title,jieshuo)
+}
 
 
 }
